559d46b15215d3c38889eeab039753ee19d33d7c
Anwesh43 <anweshthecool0@gmail.com>
2019-10-19 09:32:02 +0530

    adding RotBallMoveView class

--- rotmoveballview/src/main/java/com/anwesh/uiprojects/rotmoveballview/RotBallMoveView.kt
+++ rotmoveballview/src/main/java/com/anwesh/uiprojects/rotmoveballview/RotBallMoveView.kt
@@ -52,3 +52,21 @@ fun Canvas.drawRBMNode(i : Int, scale : Float, paint : Paint) {
     drawRotBallMove(size, w, scale, paint)
     restore()
 }
+
+class RotBallMoveView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
