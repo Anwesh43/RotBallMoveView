05bcecdafdb239b3910146dfdcb7e24765cda9da
Anwesh43 <anweshthecool0@gmail.com>
2019-10-19 10:52:12 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedrotmoveballview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrotmoveballview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedrotmoveballview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.rotmoveballview.RotBallMoveView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         RotBallMoveView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
