4d0dfe64c72c51f0c2eee37bc01be15ad96e0453
Anwesh43 <anweshthecool0@gmail.com>
2019-10-19 10:51:33 +0530

    adding RotMoveBallView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrotmoveballview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrotmoveballview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedrotmoveballview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.rotmoveballview.RotBallMoveView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RotBallMoveView.create(this)
     }
 }
