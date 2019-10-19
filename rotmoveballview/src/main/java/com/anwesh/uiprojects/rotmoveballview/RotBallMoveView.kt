4601f30bd618e7c979dc40f1b0464ce07ec98891
Anwesh43 <anweshthecool0@gmail.com>
2019-10-19 10:43:20 +0530

    adding RotBallMove class

--- rotmoveballview/src/main/java/com/anwesh/uiprojects/rotmoveballview/RotBallMoveView.kt
+++ rotmoveballview/src/main/java/com/anwesh/uiprojects/rotmoveballview/RotBallMoveView.kt
@@ -159,4 +159,28 @@ class RotBallMoveView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RotBallMove(var i : Int) {
+
+        private val root : RBMNode = RBMNode(0)
+        private var curr : RBMNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
