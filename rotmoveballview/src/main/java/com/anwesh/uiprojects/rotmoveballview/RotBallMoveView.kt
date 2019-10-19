0e65f377064a95364a7eda01641d1914be0f6651
Anwesh43 <anweshthecool0@gmail.com>
2019-10-19 10:41:22 +0530

    adding RBMNode class

--- rotmoveballview/src/main/java/com/anwesh/uiprojects/rotmoveballview/RotBallMoveView.kt
+++ rotmoveballview/src/main/java/com/anwesh/uiprojects/rotmoveballview/RotBallMoveView.kt
@@ -117,4 +117,46 @@ class RotBallMoveView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RBMNode(var i : Int, val state : State = State()) {
+
+        private var next : RBMNode? = null
+        private var prev : RBMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = RBMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRBMNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RBMNode {
+            var curr : RBMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
