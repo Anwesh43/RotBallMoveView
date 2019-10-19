4c05dc9a83f70c67cdacf9baf1406f24ac982a3d
Anwesh43 <anweshthecool0@gmail.com>
2019-10-19 08:27:12 +0530

    adding constants and libraries

--- rotmoveballview/src/main/java/com/anwesh/uiprojects/rotmoveballview/RotBallMoveView.kt
+++ rotmoveballview/src/main/java/com/anwesh/uiprojects/rotmoveballview/RotBallMoveView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.rotmoveballview
 /**
  * Created by anweshmishra on 19/10/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val nodes : Int = 5
+val scGap : Float = 0.01f
+val delay : Long = 30
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
